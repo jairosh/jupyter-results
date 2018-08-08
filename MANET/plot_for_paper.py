0c4fb14ef83fc9eb46407c9588bd8465ddf77989
Jairo Sanchez <jairojsh@gmail.com>
2018-08-08 17:14:36 -0500

    Modified styles

--- MANET/plot_for_paper.py
+++ MANET/plot_for_paper.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import AxesGrid
+from matplotlib.lines import Line2D
 
 colourWheel = ['#329932',
                '#ff6961',
@@ -32,7 +33,7 @@ dashesStyles = [[3, 1],
                 [4, 1, 1, 1, 1, 1]]
 
 # Load the results
-df = pd.read_csv('results.csv')
+df = pd.read_csv('results_full.csv')
 
 # Filter out the unnecesary data
 # Fix the buffer size to 50MB
@@ -43,25 +44,46 @@ df = df[df.nodes <= 600]
 points = df.groupby(by=['router', 'nodes', 'message_interval']).mean()
 error = df.groupby(by=['router', 'nodes', 'message_interval']).sem() * 1.96
 
-data = points[['delivery_prob', 'latency_avg', 'overhead_ratio', 'speed_avg']].join(error[['delivery_prob', 'latency_avg', 'overhead_ratio', 'speed_avg']], rsuffix='_err')
+data = points[['delivery_prob', 'latency_avg', 'overhead_ratio']].join(error[['delivery_prob', 'latency_avg', 'overhead_ratio']], rsuffix='_err')
 
+
+print(plt.style.available)
+plt.style.use('seaborn-darkgrid')
 # fig, ax = plt.subplots(ncols=4)
-fig, axes = plt.subplots(nrows=1, ncols=4, sharey=True)
-nodes = [100, 200, 400, 600]
+fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True)
+nodes = [100, 400, 600]
 nodes.reverse()
+markers = []
+for m in Line2D.markers:
+    try:
+        if len(m) == 1 and m != ' ':
+            markers.append(m)
+    except TypeError as e:
+        pass
+print(markers)
+
+# Linestyles
+# : (·········)
+# -. (-·-·-·-·)
+# -- (--------)
+# -  (solid line)
+
+styles = ['b.-', 'r+--', 'g^-.', 'yv:', 'kx-', 'cs:', 'm<:']
+routers = ['Proposed', 'Spray n\' Wait', 'Epidemic', 'PRoPHET', 'SeeR', 'MaxPROP', 'Epidemic with Oracle']
 x = ['5,25', '25,35', '35,60', '60, 120']
-styles = ['b.-', 'r+--', 'g^-.', 'yv:', 'ks-', 'c*:']
-routers = ['BFGMP', 'SprayAndWait', 'Epidemic', 'Prophet', 'Seer']
 plt.setp(axes, xticks=[0, 1, 2, 3], xticklabels=x)
 fig.text(0.5, 0.04, 'Message creation interval (s)', ha='center')
 fig.text(0.04, 0.5, 'Packet Delivery Ratio', va='center', rotation='vertical')
-for subp in range(4):
+
+for subp in range(3):
     n = nodes.pop()
     axes[subp].set_title('n={}'.format(n))
     for idx, router in enumerate(routers):
         y = data.loc[router, n].reindex(x)
         axes[subp].plot([0, 1, 2, 3], y['delivery_prob'].as_matrix(), styles[idx])
 
+print('Showing')
 plt.legend(routers)
 plt.draw()
+plt.savefig('test.png')
 plt.show()
